597843c843aa71db19dfd739b2994e9a96ec8f7d
hannereppen <hannereppen@gmail.com>
2025-04-01 09:06:43 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MinMedisinplan from "./MinMedisinplan.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MinMedisinplan />
-  </React.StrictMode>
-);
